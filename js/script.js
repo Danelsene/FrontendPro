58f5b1109db136ebd254459c574f457af33d27fe
Danelsene <diana.lagovskaa@gmail.com>
2023-02-26 14:18:13 +0200

    prompt

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var nameUser = prompt('Please, enter your name');
+
+  if (nameUser == ''){
+    alert('Hello stranger! How are you?');
+  } else {
+    alert('Hello, ' + nameUser + '! How are you?');
+  }
