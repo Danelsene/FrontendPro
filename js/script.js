8bf82a66d91c429b15aece8343d1ea72efa3c386
Danelsene <diana.lagovskaa@gmail.com>
2023-03-01 21:28:37 +0200

    number task

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
-var nameUser = prompt('Please, enter your name');
+var numberFirst = +prompt('Please, enter any number', 10);
+var numberSecond = +prompt('Now, enter any other number', 20);
+var sign = prompt('What do you want me to do with them?', '+ - * /')
 
-// var approved = confirm('Are you sure?');
 
-  if (nameUser !== ''){
-    alert('Hello, ' + nameUser + '! How are you?');
+  if (sign === '+') {
+    let result = numberFirst + numberSecond;
+    alert(`Your result is ${result}`);
+    console.log(result);
+  } else if (sign === '-') {
+    result = numberFirst - numberSecond;
+    alert(`Your result is ${result}`);
+    console.log(result);
+  } else if (sign === '*') {
+    result = numberFirst * numberSecond;
+    alert(`Your result is ${result}`);
+    console.log(result);
+  } else if (sign == '/') {
+    result = numberFirst / numberSecond;
+    alert(`Your result is ${result}`);
+    console.log(result);
   } else {
-    alert('Hello stranger! How are you?' );
+    alert(`You made a mistake`);
+    console.log(result);
   }
