33d1fbdffae8d1e03979f6888313ac55a00b7976
Danelsene <diana.lagovskaa@gmail.com>
2023-03-05 14:45:43 +0200

    Homework Update

--- js/script.js
+++ js/script.js
@@ -1,25 +1,36 @@
-var numberFirst = +prompt('Please, enter any number', 10);
-var numberSecond = +prompt('Now, enter any other number', 20);
-var sign = prompt('What do you want me to do with them?', '+ - * /')
+var userName = prompt('Please, enter your name', "Mary");
+var userSurname = prompt('Now, enter your surname', "Collins");
+
+document.write(`<ul> <li>Full name: <b>${userName.trim()} ${userSurname.trim()}</b></li>`);
+
+//----------------------------------//
+
+var userEmail = prompt('Enter your email');
+
+let email = userEmail.toLowerCase().replaceAll(" ", "");
+
+if (email.indexOf("@") == -1) {
+  document.write(`<li>not valid email <b>${email}</b> (symbol @ not exist)</li>`);
+} else if (email.indexOf("@") == 0) {
+  document.write(`<li>not valid email <b>${email}</b> (symbol @ find in first place)</li>`);
+} else if (email.indexOf("@") == email.length - 1) {
+  document.write(`<li>not valid email <b>${email}</b> (symbol @ find in last place)</li>`);
+} else {
+  document.write(`<li>Email: ${email}</li>`);
+}
+
+//------------------------------------------------------------//
+
+var userAge = prompt('Please, enter your year of birth', 1990);
+
+let clearAge = userAge.replaceAll(" ", "");
+let today = new Date().getFullYear();
+let age = today - clearAge;
+document.write(`<li>Age: <b>${age}</b> </li>`);
 
 
-  if (sign === '+') {
-    let result = numberFirst + numberSecond;
-    alert(`Your result is ${result}`);
-    console.log(result);
-  } else if (sign === '-') {
-    result = numberFirst - numberSecond;
-    alert(`Your result is ${result}`);
-    console.log(result);
-  } else if (sign === '*') {
-    result = numberFirst * numberSecond;
-    alert(`Your result is ${result}`);
-    console.log(result);
-  } else if (sign == '/') {
-    result = numberFirst / numberSecond;
-    alert(`Your result is ${result}`);
-    console.log(result);
-  } else {
-    alert(`You made a mistake`);
-    console.log(result);
-  }
+
+
+
+
+
