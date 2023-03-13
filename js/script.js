31d539cd6214efb546825760cb45620db7a44ee2
Danelsene <diana.lagovskaa@gmail.com>
2023-03-13 13:08:52 +0200

    factorialllllll

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
-let firstNumber = +prompt('Enter first number', 10);
-let secondNumber = +prompt('Enter second number', 20);
-let thirdNumber = +prompt('Enter third number', 30);
-let average = (firstNumber + secondNumber + thirdNumber) / 3;
+let a = +prompt('Enter first number', 1);
+let b = +prompt(`Enter second number that should be bigger than ${a}`);
+let h = +prompt(`Enter a step value that is bigger than 0`);
+let sumOfFactorials = 0;
 
-if(isNaN(firstNumber) || isNaN(secondNumber) || isNaN(thirdNumber)){
+if (isNaN(a) || isNaN(b) || isNaN(h)){
   alert(`You entered not a number, please update the page and try again`);
-} else {
-  alert(`The average of your numbers is ${average}`);
+} 
+
+if (a > b){
+  b = +prompt(`Your second number is smaller than first, enter your second number again(it should be bigger than ${a})`)
+} 
+
+if (h <= 0){
+  h = +prompt(`Your step value is smaller than 0, please enter any other one bigger than 0`);
 }
 
-
-
+  for (let i = a; i <= b; i += h){
+    let factorial = 1;
+      for (let j = 2; j <= i; j++) {
+        factorial *= j;
+      }
+    sumOfFactorials += factorial; 
+  }
+  console.log(sumOfFactorials);
