45d4d05a1b5b160a941079d03cbbe9d793ba3200
Danelsene <diana.lagovskaa@gmail.com>
2023-03-17 15:13:40 +0200

    questionaire

--- js/script.js
+++ js/script.js
@@ -1,26 +1,42 @@
-let a = +prompt('Enter first number', 1);
-let b = +prompt(`Enter second number that should be bigger than ${a}`);
-let h = +prompt(`Enter a step value that is bigger than 0`);
-let sumOfFactorials = 0;
+let firstAnswer = +prompt("Скільки буде 2 + 2?");
+let sum = 0;
 
-if (isNaN(a) || isNaN(b) || isNaN(h)){
-  alert(`You entered not a number, please update the page and try again`);
-} 
-
-if (a > b){
-  b = +prompt(`Your second number is smaller than first, enter your second number again(it should be bigger than ${a})`)
-} 
-
-if (h <= 0){
-  h = +prompt(`Your step value is smaller than 0, please enter any other one bigger than 0`);
+if (firstAnswer === null) {
+    sum += 0;
+} else if (firstAnswer == "4") {
+    sum += 10;
 }
 
-  for (let i = a; i <= b; i += h){
-    let factorial = 1;
-      for (let j = 2; j <= i; j++) {
-        factorial *= j;
-      }
-    sumOfFactorials += factorial; 
-  }
-  console.log(sumOfFactorials);
-  document.write(`Your sum of factorials equals ${sumOfFactorials}`)
+let secondAnswer = prompt("Сонце встає на сході?");
+
+if (secondAnswer === null){
+    sum += 0;
+} else if (secondAnswer.trim().toLowerCase() == "так" || secondAnswer.trim().toLowerCase() == "да") {
+    sum += 10;
+}
+
+let thirdAnswer = prompt("Скільки буде 5 / 0?");
+
+if (thirdAnswer === null){
+    sum += 0;
+} else if (thirdAnswer == "0" || thirdAnswer.trim().toLowerCase() == "нуль" || thirdAnswer.trim().toLowerCase() == "ноль") {
+    sum += 10;
+}
+
+let fourthAnswer = prompt("Якого кольору небо?");
+
+if (fourthAnswer === null){
+    sum += 0;
+ } else if (fourthAnswer.trim().toLowerCase() == "блакитне" || fourthAnswer.trim().toLowerCase() == "синє" || fourthAnswer.trim().toLowerCase() == "голубе" || fourthAnswer.trim().toLowerCase() == "лазурне"){
+    sum += 10;
+}
+
+let fifthAnswer = +prompt("Яка правильна відповідь на головне питання життя, всесвіту та т.д?");
+
+if (fifthAnswer === null){
+    sum += 0;
+ } else if (fifthAnswer == "42") {
+    sum += 10;
+}
+
+alert(sum);
