1b3c73215014cb971f1ee05466a503b222117131
Danelsene <diana.lagovskaa@gmail.com>
2023-04-02 16:41:24 +0300

    removeEl

--- js/script.js
+++ js/script.js
@@ -1,37 +1,14 @@
-const a = 2;
-const b = 3;
+const array = [1, 2, 3, 4, 5, 6, 7];
 
-function sum (a, b) {
-    return a + b;
-}
-
-    sum(a, b);
-    console.log(sum(a, b));
-    document.write(sum(a, b)); //----print sum result
-
-    document.write(`<hr/>`)
-
-let c = function (any) {
-    return any - 2;
-}
-
-    console.log(typeof(c)) //----check c type
-    c(sum(a, b));
-    console.log(c(sum(a, b)));
-    document.write(c(sum(a, b))); //------ rusult of c function
-
-    document.write(`<hr/>`)
-
-function main (a, b, c) { 
-    if(typeof(c) === 'function') {
-        return c(sum(a, b)) -2; //---тут додала -2 щоб перевірити чи спрацьовує перевірка на функцію
+function remove (arr, a){
+    const index = arr.indexOf(a);
+    if(index === -1) {
+        console.log(`There is no such an element as ${a}`);
     } else {
-        return sum(a, b);
+        return arr.splice(index, 1);
     }
 }
 
-    main(a, b, c);
-    console.log(main(a, b, c));
-    document.write(main(a, b, c)); //-------result of main function
+remove(array, 5);
 
-    document.write(`<hr/>`)
+console.log(array);
