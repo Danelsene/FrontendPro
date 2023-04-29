4afce89d76b4d3af2617263d2774520562d59a47
Danelsene <diana.lagovskaa@gmail.com>
2023-04-29 13:35:27 +0300

    render of products

--- js/script.js
+++ js/script.js
@@ -1,87 +1,119 @@
-let users = [
-{
-"index": 0,
-"isActive": true,
-"balance": "$2,226.60",
-"name": "Eugenia Sawyer",
-"gender": "female",
-"phone": "+1 (840) 583-3207",
-"address": "949 John Street, Rose, Puerto Rico, 1857"
-},
-{
-"index": 1,
-"isActive": true,
-"balance": "$2,613.77",
-"name": "Pauline Gallegos",
-"gender": "female",
-"phone": "+1 (985) 593-3328",
-"address": "328 Greenpoint Avenue, Torboy, North Dakota, 6857"
-},
-{
-"index": 2,
-"isActive": false,
-"balance": "$3,976.41",
-"name": "Middleton Chaney",
-"gender": "male",
-"phone": "+1 (995) 591-2478",
-"address": "807 Fleet Walk, Brutus, Arkansas, 9783"
-},
-{
-"index": 3,
-"isActive": true,
-"balance": "$1,934.58",
-"name": "Burns Poole",
-"gender": "male",
-"phone": "+1 (885) 559-3422",
-"address": "730 Seba Avenue, Osage, Alabama, 6290"
-},
-{
-"index": 4,
-"isActive": true,
-"balance": "$3,261.65",
-"name": "Mcfadden Horne",
-"gender": "male",
-"phone": "+1 (942) 565-3988",
-"address": "120 Scholes Street, Kirk, Michigan, 1018"
-},
-{
-"index": 5,
-"isActive": false,
-"balance": "$1,790.56",
-"name": "Suzette Lewis",
-"gender": "female",
-"phone": "+1 (837) 586-3283",
-"address": "314 Dunne Place, Bawcomville, Guam, 9053"
+let kitchenProducts = [
+	{
+		type: 'grater',
+		price: 10
+	},
+	{
+		type: 'pastry-bag',
+		price: 25
+	},
+	{
+		type: 'scale',
+		price: 5
+	},
+	{
+		type: 'whisk',
+		price: 15
+	}
+];
+
+let devicesProducts = [
+	{
+		type: 'desktop',
+		price: [100,1000]
+	},
+	{
+		type: 'laptop',
+		price: [50,1500]
+	},
+	{
+		type: 'smartphone',
+		price: [80,2000]
+	},
+	{
+		type: 'tablet',
+		price: [20,1300]
+	}
+];
+
+let cosmeticsProducts = [
+	{
+		type: 'blush',
+		price: 100
+	},
+	{
+		type: 'eyeshadow',
+		price: 50
+	},
+	{
+		type: 'lipstick',
+		price: 80
+	},
+	{
+		type: 'nail-polish',
+		price: 200
+	},
+	{
+		type: 'perfume',
+		price: 300,
+	}
+];
+
+let Kitchen = {
+    category: 'Kitchen'
+};
+
+let Devices = {
+    category: 'Devices'
+};
+
+let Cosmetics = {
+    category: 'Cosmetics'
+};
+
+let modProducts = [];
+
+let getProto = (arr, proto) => {
+    modProducts = arr
+    .map(products => {
+        let newProducts = Object.create(proto);
+        for(let key in products) {
+            newProducts[key] = products[key];
+        }
+        return newProducts;
+    })
+    return modProducts;
 }
+
+arr = [
+    getProto(kitchenProducts, Kitchen),
+    getProto(devicesProducts, Devices),
+    getProto(cosmeticsProducts, Cosmetics)
 ]
 
+// console.log(modProducts)
 
-let phones = [];
+let renderArr = [];
+let newarr = [];
 
-users.forEach(user => {
-    if (user.balance[1] >= 2) {phones.push(user.phone)};
-    return phones;
+arr.forEach(element => {
+    element.map(obj => {
+		console.log(obj.category)
+        renderArr.push(`<div class="category_box">
+                            <div class="category_img">
+                                <img src="img/${obj.type}.svg" alt="">
+                            </div>
+                            <div class="category_title">
+                                <p class="title_name">Name:
+                                    <span> ${obj.type}</span>
+                                </p>
+                                <p class="title_price">Price:
+                                    <span> $${Array.isArray(obj.price) === true ? String(obj.price[0])+-+String(obj.price[1]) : obj.price}</span>
+                                </p>
+                            </div>
+                        </div>`)
+    })
+    document.write(`<div class="category"> <span class="category__main_title"> Category: ${element[0].category} </span> ${renderArr.join('')}</div>`);
+	console.log(renderArr.join(''))
+    renderArr.splice(0);
 })
-
-console.log(phones);
-document.write(phones); //----------------вивели номера телефонів
-
-
-let balances = [];
-
-users.forEach(user => {
-    balances.push(user.balance);
-    return balances;
-})
-
-console.log(balances); //--------------------отримали масив всіх балансів
-
-let balance = 0;
-
-balances.forEach(number => {
-    balance += parseFloat(number.replace(/[$,]/g, ''))
-})
-
-console.log(balance.toFixed(2));
-document.write(`<br/>`)
-document.write(`Sum of the balances is ${balance.toFixed(2)}`) //----------------перетворили баланси на числа та додали
