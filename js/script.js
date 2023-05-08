871f66cc0434de4b123b4e36d3b782e70ff47984
Danelsene <diana.lagovskaa@gmail.com>
2023-05-08 21:25:35 +0300

    key generator

--- js/script.js
+++ js/script.js
@@ -1,61 +1,12 @@
-// let human = {
-// 	name: this.name,
-// 	age: this.age,
-// 	constructor: function(name, age){
-// 		return `My name is ${name} and I'm ${age} years old`
-// 	},
-// 	getConsole: function(){
-// 		console.log(this);
-// 	}
-// }
-
-// console.log(human.constructor('Baby', 14));
-// console.log(human.getConsole());
-
-
-class Human {
-	constructor(name, age) {
-	  this.name = name;
-	  this.age = age;
+function generateKey(length, characters) {
+	let result = '';
+	const charactersLength = characters.length;
+	for (let i = 0; i < length; i++) {
+	  result += characters.charAt(Math.floor(Math.random() * charactersLength));
 	}
-	getInfo() {
-	  console.log(`Name: ${this.name}, Age: ${this.age}`);
-	}
-  }
-
-  console.log(new Human("Mary", 35));
-  
-
-  class Car {
-	constructor(brand, model, year, number, owner) {
-	  this.brand = brand;
-	  this.model = model;
-	  this.year = year;
-	  this.number = number;
-	 	this.owner = owner;
-	}
-	getInfo() {
-	  console.log(`Brand: ${this.brand}, Model: ${this.model}, Year: ${this.year}, Number: ${this.number}`);
-	  if (this.owner.age < 18) {
-		console.log('Sorry, the owner should be older than 18');
-	  } else {
-		this.owner.getInfo();
-	}
-  	}
+	return result;
 }
-  
-	const human1 = new Human('John', 25);
-	console.log(human1);
-	const human2 = new Human('Mary', 16);
-	console.log(human2);
-	const car1 = new Car('Toyota', 'Corolla', 2021, 'AB1234', human1);
-	console.log(car1);
-	const car2 = new Car('Honda', 'Civic', 2019, 'CD5678', human2);
-	console.log(car2);
-
-
-		car1.owner = human1;
-		car2.owner = human2;
-	
-	car1.getInfo();
-	car2.getInfo();
+let someCharacters = 'isdvndljabcjhabcbabcl45t3';  
+const key = generateKey(5, someCharacters);
+console.log(`Our key is ${key}`);
+document.write(`Our key is ${key}`);
