cf586b7bb1e960421cbf373a1765064c08ad19a1
Danelsene <diana.lagovskaa@gmail.com>
2023-05-09 11:09:07 +0300

    colored block movement

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
-function generateKey(length, characters) {
-	let result = '';
-	const charactersLength = characters.length;
-	for (let i = 0; i < length; i++) {
-	  result += characters.charAt(Math.floor(Math.random() * charactersLength));
-	}
-	return result;
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
-let someCharacters = 'isdvndljabcjhabcbabcl45t3';  
-const key = generateKey(5, someCharacters);
-console.log(`Our key is ${key}`);
-document.write(`Our key is ${key}`);
+
+setInterval(() => {
+  const block = document.querySelector('.block');
+  block.style.backgroundColor = getRandomColor();
+}, 500);
+
+setInterval(() => {
+  const block = document.querySelector('.block');
+  const bodyWidth = window.innerWidth;
+  const bodyHeight = window.innerHeight;
+  const newLeft = Math.floor(Math.random() * bodyWidth);
+  const newTop = Math.floor(Math.random() * bodyHeight);
+  block.style.left = newLeft + 'px';
+  block.style.top = newTop + 'px';
+}, 1000);
+
+
+
+
+
+
