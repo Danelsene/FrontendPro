7b24e6613071e8324973c0249ab0a57405eab4bd
Danelsene <diana.lagovskaa@gmail.com>
2023-05-30 01:35:17 +0300

    firstpagerally

--- js/script.js
+++ js/script.js
@@ -1,26 +1,9 @@
-function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+const startButton = document.getElementById('start');
+const rallyMenu = document.getElementById('rallymenu')
 
-setInterval(() => {
-  const block = document.querySelector('.block');
-  block.style.backgroundColor = getRandomColor();
-}, 500);
-
-setInterval(() => {
-  const block = document.querySelector('.block');
-  const bodyWidth = window.innerWidth;
-  const bodyHeight = window.innerHeight;
-  const newLeft = Math.floor(Math.random() * bodyWidth);
-  const newTop = Math.floor(Math.random() * bodyHeight);
-  block.style.left = newLeft + 'px';
-  block.style.top = newTop + 'px';
-}, 1000);
+startButton.addEventListener('click', function() {
+  rallyMenu.style.visibility = 'visible';
+});
 
 
 
